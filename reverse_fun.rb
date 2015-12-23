ede74706be0e54a1c26fb2229592f5ffbe0d85a6
wolfieorama <wachirawamunyi@gmail.com>
2015-12-24 00:21:37 +0300

    finito

--- reverse_fun.rb
+++ reverse_fun.rb
@@ -7,16 +7,17 @@ def reverse_fun(n)
   #4 .repeat till its done
   #output is a string
   storage_n = []
-  new_n = n.split("").reverse
-  i = 0
-    while i <= new_n.length
-      storage_n.push(new_n.pop(i))
-      new_n = new_n.reverse
-      i += 1
-    end
-    return storage_n.join("")
+  new_n = n.split("")
+  i = new_n.length
+  while i > 0
+    storage_n.push(new_n.pop(1))
+    new_n = new_n.reverse
+    i -= 1
+  end
+  return storage_n.join("")
 end
 
-  p reverse_fun('012') #'201')
+  p reverse_fun('012') #'201') 210 - 2
+
 	p reverse_fun('012345') #'504132')
 	p reverse_fun('0123456789') #'9081726354')
