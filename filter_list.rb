491b8e509cca8472ac084ad205139b4c4f668577
wolfieorama <wachirawamunyi@gmail.com>
2015-12-27 23:24:39 +0300

    added tests

--- filter_list.rb
+++ filter_list.rb
@@ -5,14 +5,15 @@ def filter_list(l)
   #2. if its an number leave it if its a string push to the end of the array and pop it
   # output return a new list with the strings filtered out
 
-  new_l= []
+    new_l= []
 
-  l.each do |x|
-    if x.is_a? Numeric
-      new_l.push(x)
-    end
-  return new_l
-end
+    l.each do |x|
+      if x.is_a? Numeric
+        new_l.push(x)
+      end
+    return new_l
+  end
 end
 
-p [1,2,'a','b']
+p [1,2,'a','b'] #[1,2]
+p [1,3,4,'g','h'] #[1,3,4]
