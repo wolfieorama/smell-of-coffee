9865d95a3db7c67d0176cce8e9657fb8767ff71a
wolfieorama <wachirawamunyi@gmail.com>
2016-01-28 23:58:41 +0300

    added further tests

--- isTriangle.rb
+++ isTriangle.rb
@@ -1,12 +1,12 @@
 def isTriangle(a,b,c)
-  #a2 +b2 = c2
-  #check if the integers certify the above criteria
+
   a2 = a*a
   b2 = b*b
   c2 = c*c
 
   if a2 + b2 == c2
     return true
+
   else
     return false
   end
@@ -14,3 +14,5 @@ def isTriangle(a,b,c)
 end
 
 p isTriangle(3,4,5)
+p isTriangle(2,4,5)
+p isTriangle(9,16,125)
